5c212ffd9ffd1e1833e7c1f0bd6ab45aa09ed8ac
Roberto Hinz <robhz786@gmail.com>
2026-03-11 19:45:49 -0300

    Implement Undo

--- quaternity_clock.js
+++ quaternity_clock.js
@@ -15,13 +15,16 @@ function playBeep(duration_sec) {
     oscillator.stop(audioCtx.currentTime + duration_sec);
 }
 
+
 class QuaternityPlayerTimeCounter {
     constructor(time_sec, additionPerRound_sec) {
         this.onGame = true;
         this._remainingTime_ms = time_sec * 1000.0;
         this._additionPerRound_ms = additionPerRound_sec * 1000.0;
     }
-
+    SetRemainingTime_ms(t) {
+        this._remainingTime_ms = t;
+    }
     RemainingTime_ms() {
         return this._remainingTime_ms;
     }
@@ -42,8 +45,181 @@ class QuaternityPlayerTimeCounter {
     }
 }
 
+function PlayerName(playerIdx) {
+    switch(playerIdx) {
+        case 0: return "White";
+        case 1: return "Red";
+        case 2: return "Black";
+        case 3: return "Green";
+        default: return "Invalid player";
+    }
+}
+
+class RecoverableGameState {
+    constructor(playerTimeCounterArr, turnIdx) {
+        this.players = [];
+        for (let p of playerTimeCounterArr) {
+            this.players.push({
+                onGame: p.onGame,
+                remainingTime_ms: p.RemainingTime_ms()
+            });
+        }
+        this.playerIdx = turnIdx;
+    }
+}
+
+const GameEvents = Object.freeze({
+    PLAYER_MOVED: 'PLAYER_MOVED',
+    PLAYER_SPENT_TIME: 'PLAYER_SPENT_TIME',
+    PLAYER_LOST_BY_TIMEOUT: 'PLAYER_LOST_BY_TIMEOUT',
+    PLAYER_REMOVED: 'PLAYER_REMOVED'
+});
+
+
+class QuaternityGameHistory {
+
+    constructor(initialState) {
+        this._movesHistory = [];
+        this._statesHistory = [initialState];
+    }
+
+//    Start(playerIdx, playersState) {
+//        this._statesHistory = [playersState];
+//        this._movesHistory = [];
+//        this._elapsedTimeSiceLastMove = 0;
+//    }
+
+
+    CurrentPlayerIdx() {
+        return this._statesHistory.at(-1).playerIdx;
+    }
+
+    CurrentPlayerName() {
+        return PlayerName(this.CurrentPlayerIdx());
+    }
+
+    RegisterPlayerMove(newPlayersState) {
+        const state = this._statesHistory.at(-1);
+        //if (state.players.at(state.playerIdx).onGame) {
+        const txt = `${this.CurrentPlayerName()} -> ${PlayerName(newPlayersState.playerIdx)}`;
+        this._movesHistory.push({
+            msg: function () { return txt; },
+            eventType: GameEvents.PLAYER_MOVED
+        });
+        this._statesHistory.push(newPlayersState);
+    }
+
+    RegisterElapsedTime(elapsedTime_ms) {
+        if (this._movesHistory.at(-1)?.eventType === GameEvents.PLAYER_SPENT_TIME) {
+            this._UpdateCurrentSpentTimeEvent(elapsedTime_ms);
+        } else {
+            this._PushNewSpentTimeEvent(elapsedTime_ms)
+        }
+    }
+
+    _UpdateCurrentSpentTimeEvent(elapsedTime_ms) {
+        this._movesHistory.at(-1).spentTime += elapsedTime_ms;
+        const currentState = this._statesHistory.at(-1);
+        const playerIdx = currentState.playerIdx;
+        currentState.players.at(playerIdx).remainingTime_ms -= elapsedTime_ms;
+
+        //console.debug("Add spent time (%f)", elapsedTime_ms);
+    }
+
+    _PushNewSpentTimeEvent(elapsedTime_ms) {
+        let currentState = this._statesHistory.at(-1);
+        const playerIdx = currentState.playerIdx;
+
+        this._movesHistory.push( {
+            msg: function() {
+                const secondsStr = (0.001 * this.spentTime).toFixed(3);
+                return `${this.playerName} spent ${secondsStr} seconds`;
+            },
+            eventType: GameEvents.PLAYER_SPENT_TIME,
+            spentTime: elapsedTime_ms,
+            playerName: PlayerName(playerIdx)
+        });
+
+        const newState = structuredClone(currentState);
+        newState.players.at(playerIdx).remainingTime_ms -= elapsedTime_ms;
+        this._statesHistory.push(newState);
+
+        //console.debug("Start spent time (%f)", elapsedTime_ms);
+    }
+
+    RegisterPlayerTimeout(newState) {
+        // ( we must call CurrentPlayerName() before doing _statesHistory.pop() )
+        const newEventObject = this._MakePlayerTimeoutEventObject(this.CurrentPlayerName());
+
+        if (this._movesHistory.at(-1)?.eventType === GameEvents.PLAYER_SPENT_TIME) {
+            // this sould always be the case, i supose
+            this._movesHistory.pop();
+            this._statesHistory.pop();
+        }
+        this._movesHistory.push(newEventObject);
+        this._statesHistory.push(newState);
+    }
+
+    _MakePlayerTimeoutEventObject(playerName) {
+        const msg = `${playerName} lost by timeout`;
+        return {
+            msg: function() { return msg; },
+            eventType: GameEvents.PLAYER_LOST_BY_TIMEOUT
+        };
+    }
+
+    RegisterPlayersRemoval(removedPlayers, newPlayersState) {
+        this._movesHistory.push(this._MakePlayersRemovalEventObject(removedPlayers));
+        this._statesHistory.push(newPlayersState);
+    }
+
+    _MakePlayersRemovalEventObject(removedPlayers) {
+        const txt = this._PlayersRemovalMsg(removedPlayers);
+        return {
+            msg: function () { return txt; },
+            eventType: GameEvents.PLAYER_REMOVED
+        };
+    }
+
+    _PlayersRemovalMsg(removedPlayers) {
+        let txt = "";
+        for (const p in removedPlayers) {
+            const separator = p == 0 ? " " : p < (removedPlayers.length - 1) ? ", " : " and ";
+            txt = txt.concat(separator, PlayerName(removedPlayers[p]));
+        }
+        return txt.concat(" removed");
+    }
+
+    GetGameStateAt(idx) {
+        return this._statesHistory.at(idx);
+    }
+
+    GetMove(idx) {
+        return this._movesHistory.at(idx);
+    }
+    GetMoveDescription(idx) {
+        return this._movesHistory.at(idx).msg();
+    }
+
+    MovesCount() {
+        return this._movesHistory.length;
+    }
+
+    Rollback(count) {
+        const oldSize = this._movesHistory.length
+        if (count >= oldSize) {
+            this._movesHistory.length = 0;
+            this._statesHistory.length = 1;
+        } else if (count > 0) {
+            this._movesHistory.length = oldSize - count;
+            this._statesHistory.length = oldSize - count + 1;
+        }
+    }
+}
+
+
 class QuaternityClock {
-    constructor(players, statusUI, buttonsSet) {
+    constructor(players, statusUI, buttonsSet, history) {
         this._players = players;
         this._ui = statusUI;
         for (let i in this._players) {
@@ -60,6 +236,13 @@ class QuaternityClock {
         this._thresholdTimestamp = 0.0;
         this._ThresholdTimeCallback = null;
         this._buttonsSet = buttonsSet;
+        //this._SaveCurrentStateInHistory();
+        this._history = history;
+        this._playersRemovalToBeRegistered = new Set();
+    }
+
+    _CurrentHistoryState() {
+        return new RecoverableGameState(this._players, this._currentPlayerIdx);
     }
 
     _RequestAnimationFrame()
@@ -70,6 +253,9 @@ class QuaternityClock {
 
     _AnimNewFrameCallback(timestamp) {
         if (timestamp >= this._thresholdTimestamp) {
+            if (this._previousTimestamp === 0.0) { // means first time this function is called
+                this._previousTimestamp = timestamp; // starting timestamp;
+            }
             this._ThresholdTimeCallback(timestamp);
         } else if (this._IsCurrentPlayerButtonPressed()) {
             this._OnPlayerButtonPressed(timestamp);
@@ -83,9 +269,14 @@ class QuaternityClock {
     }
 
     _OnPlayerButtonPressed(timestamp) {
-        const remainingTime = this._currentPlayer.OnMoveDone(timestamp - this._previousTimestamp);
-        this._ui.UpdatePlayerRemainingTime(this._currentPlayerIdx, remainingTime);
-        this._MoveToNextPlayer();
+        const elapsedTime = timestamp - this._previousTimestamp;
+        this._history.RegisterElapsedTime(elapsedTime);
+        this._previousTimestamp = timestamp;
+
+        this._SwitchToNextPlayer(); //this.NextActivePlayerIdx();
+        const newState = this._CurrentHistoryState();
+        this._history.RegisterPlayerMove(newState);
+        this._ui.UpdatePlayersClock(newState);
 
         this._Run(timestamp);
     }
@@ -97,32 +288,43 @@ class QuaternityClock {
 
     _DoPause(timestamp) {
         const elapsedTime = timestamp - this._previousTimestamp;
+        this._history.RegisterElapsedTime(elapsedTime);
+        this._previousTimestamp = timestamp;
+
         const remainingTime = this._currentPlayer.SubtractTime(elapsedTime);
         if (remainingTime <= 0.0) {
             this._OnCurrentPlayerTimeOut();
         } else {
-            let nextPlayerIdx = this.NextActivePlayerIdx(this._currentPlayerIdx);
+            let nextPlayerIdx = this.NextActivePlayerIdx();
             this._ui.AlertPause();
         }
     }
 
-    Unpause(playerIdx) {
-        if (playerIdx != this._currentPlayerIdx) {
-            if (this._currentPlayer.onGame) {
-                // add the extra time per move:
-                const remainingTime_ms = this._currentPlayer.OnMoveDone(0);
-                this._ui.UpdatePlayerRemainingTime(this._currentPlayerIdx, remainingTime_ms);
-            }
-            this._SwitchToPlayer(playerIdx)
-        }
-        this.UnpauseOnCurrentPlayer();
-    }
-
     UnpauseOnCurrentPlayer() {
+        this._RegisterPlayersRemovalIfPending();
         this._ui.AlertRunning();
         this._thresholdTimestamp = 0;
         this._ThresholdTimeCallback = (timestamp)=>{ this._Run(timestamp)};
         this._RequestAnimationFrame();
+    }
+
+    UnpauseOnNextPlayer() {
+        this._RegisterPlayersRemovalIfPending();
+
+        this._SwitchToNextPlayer(); // this.NextActivePlayerIdx()
+        const newState = this._CurrentHistoryState();
+        this._history.RegisterPlayerMove(newState);
+        this._ui.UpdatePlayersClock(newState);
+
+        this.UnpauseOnCurrentPlayer();
+    }
+
+    _RegisterPlayersRemovalIfPending() {
+        if (this._playersRemovalToBeRegistered.size > 0) {
+            const arr = Array.from(this._playersRemovalToBeRegistered);
+            this._playersRemovalToBeRegistered.clear();
+            this._history.RegisterPlayersRemoval(arr, this._CurrentHistoryState() );
+        }
     }
 
     DisablePlayer(idx) {
@@ -131,6 +333,7 @@ class QuaternityClock {
                 this._players[idx].onGame = false;
                 --this._activePlayersCount;
                 this._ui.DisablePlayer(idx);
+                this._playersRemovalToBeRegistered.add(idx);
             }
         }
     }
@@ -140,6 +343,7 @@ class QuaternityClock {
             if (!this._players[idx].onGame) {
                 this._players[idx].onGame = true;
                 ++this._activePlayersCount;
+                this._playersRemovalToBeRegistered.delete(idx);
                 if (idx == this._currentPlayerIdx) {
                     this._ui.HighlightPlayer(idx);
                 } else {
@@ -150,25 +354,28 @@ class QuaternityClock {
     }
 
     PlayersAvailableForUnpausing() {
-        if (this._activePlayersCount >= 2) {
-            const nextPlayerIdx = this.NextActivePlayerIdx(this._currentPlayerIdx);
-            if (this._currentPlayer.onGame) {
-                return [this._currentPlayerIdx, nextPlayerIdx];
-            }
-            return [nextPlayerIdx];
+        if (this._activePlayersCount < 2) { // game is over
+            return {};
         }
-        return [];
+        if (this._history.GetMove(-1)?.eventType === GameEvents.PLAYER_SPENT_TIME) {
+            return {
+                current: this._currentPlayerIdx,
+                next: this.NextActivePlayerIdx() };
+        }
+        return { current: this._currentPlayerIdx  };
     }
 
     _WhenCurrentPlayerDisplayedTimeMustBeIncremented(timestamp) {
         let elapsedTime = timestamp - this._previousTimestamp;
+        this._history.RegisterElapsedTime(elapsedTime);
         this._previousTimestamp = timestamp;
-        console.debug("----------------------------------------------------");
-        console.debug("player[%d] time before = %f ms",
-                      this._currentPlayerIdx,
-                      this._currentPlayer.RemainingTime_ms());
+
+        //console.debug("----------------------------------------------------");
+        //console.debug("player[%d] time before = %f ms",
+        //              this._currentPlayerIdx,
+        //              this._currentPlayer.RemainingTime_ms());
         const remainingTime_ms = this._currentPlayer.SubtractTime(elapsedTime);
-        console.debug("player[%d] time after = %f", this._currentPlayerIdx, remainingTime_ms);
+        //console.debug("player[%d] time after = %f", this._currentPlayerIdx, remainingTime_ms);
 
         if (remainingTime_ms <= 0) {
             this._OnCurrentPlayerTimeOut();
@@ -179,6 +386,7 @@ class QuaternityClock {
     }
 
     _Run(timestamp) {
+        //this._history.RegisterElapsedTime(timestamp - this._previousTimestamp);
         this._previousTimestamp = timestamp;
         this._thresholdTimestamp = timestamp + this._HowLongToUpdateCurrentPlayerTimeAgain();
         this._ThresholdTimeCallback =
@@ -197,30 +405,42 @@ class QuaternityClock {
 
     _OnCurrentPlayerTimeOut() {
         playBeep(1.0);
-        this._ui.SetPlayerRemainingTimeToZero(this._currentPlayerIdx);
+        const playerIdx = this._currentPlayerIdx;
+        this._ui.SetPlayerRemainingTimeToZero(playerIdx);
         this._currentPlayer.onGame = false;
         --this._activePlayersCount;
         const gameOver = this._activePlayersCount < 2;
-        this._ui.AlertPlayerLostByTime(this._currentPlayerIdx, gameOver);
-    }
-
-    _MoveToNextPlayer() {
-        this._SwitchToPlayer(this.NextActivePlayerIdx(this._currentPlayerIdx));
-    }
-
-    _SwitchToPlayer(playerIdx) {
-        if (this._currentPlayer.onGame) {
-            this._ui.UnhighlightPlayer(this._currentPlayerIdx);
-        } else {
-            this._ui.DisablePlayer(this._currentPlayerIdx);
+        if (! gameOver) {
+            this._SwitchToNextPlayer();
         }
-        this._currentPlayerIdx = playerIdx;
-        this._currentPlayer = this._players[this._currentPlayerIdx];
-        this._ui.HighlightPlayer(this._currentPlayerIdx);
+        const newState = this._CurrentHistoryState();
+        this._history.RegisterPlayerTimeout(newState);
+        this._ui.UpdatePlayersClock(newState);
+
+        const playersForUnpausing = {current: this._currentPlayerIdx};
+        this._ui.AlertPlayerLostByTime(playerIdx, gameOver, playersForUnpausing);
     }
 
-    NextActivePlayerIdx(idx) {
+    _SwitchToNextPlayer() {
+        const playerIdx = this.NextActivePlayerIdx();
+        if (playerIdx !== null) {
+            if (this._currentPlayer.onGame) {
+                this._currentPlayer.OnMoveDone(0);
+            }
+            this._currentPlayerIdx = playerIdx;
+            this._currentPlayer = this._players[this._currentPlayerIdx];
+            //this._history.RegisterPlayerMove(newState);
+            //this._ui.UpdatePlayersClock(newState);
+        }
+    }
+
+    NextActivePlayerIdx() {
+        let idx = this._currentPlayerIdx;
+        let count = 0;
         do {
+            if (++count === 4) {
+                return undefined;
+            }
             idx++
             if (idx == this._players.length) {
                 idx = 0;
@@ -235,6 +455,32 @@ class QuaternityClock {
 
     IsPlayerActive(idx) {
         return this._players[idx].onGame;
+    }
+
+    Undo(count) {
+        this._history.Rollback(count);
+        const state = this._history.GetGameStateAt(-1);
+        this._currentPlayerIdx = state.playerIdx;
+        this._currentPlayer = this._players[this._currentPlayerIdx];
+        this._activePlayersCount = 0;
+        for (let i in state.players) {
+            this._players[i].onGame = state.players[i].onGame;
+            this._players[i].SetRemainingTime_ms(state.players[i].remainingTime_ms);
+            if (state.players[i].onGame) {
+                ++this._activePlayersCount;
+            }
+        }
+        this._ui.UpdatePlayersClock(state); // not necessary though, already done;
+
+        // return players available for unpausing
+        const lastMove = this._history.GetMove(-1);
+        if (lastMove?.eventType === GameEvents.PLAYER_SPENT_TIME) {
+            return {
+                current: this._currentPlayerIdx,
+                next: this.NextActivePlayerIdx()
+            };
+        }
+        return {current: this._currentPlayerIdx};
     }
 };
 
